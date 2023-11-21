8a1ad7709eb864d4315992f06fec2c63d31bcb71
J Read <j@8bit.coffee>
2023-11-21 14:18:32 -0600

    base mode complete

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
+import './PlantList.css';
 
 
 function PlantList() {
     const dispatch = useDispatch();
 
     const reduxState = useSelector(store => store);
+    const plantList = useSelector(store => store.plantList)
+
+    const getPlants = () => {
+        dispatch({type: "FETCH_PLANTS"})
+    };
+
+    const deletePlant = (id) => {
+        dispatch({type: "DELETE_PLANT", payload: id})
+    };
 
     useEffect(() => {
         console.log('component did mount');
+        getPlants();
         // dispatch an action to request the plantList from the API
     }, []); 
 
@@ -16,6 +27,24 @@ function PlantList() {
         <div>
             <h3>This is the plant list</h3>
             <pre>{JSON.stringify(reduxState)}</pre>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Delete</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {plantList.map((plant, index) => (
+                        <tr key={index}>
+                            <td>{plant.id}</td>
+                            <td>{plant.name}</td>
+                            <td><button onClick={()=>deletePlant(plant.id)}>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import { takeEvery, put, take } from 'redux-saga/effects';
+import axios from 'axios';
 
 import App from './App';
 
@@ -12,18 +16,67 @@ const startingPlantArray = [
   { id: 3, name: 'Oak' }
 ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
       return [ ...state, action.payload ]
+    case "SET_PLANTS":
+      return action.payload
     default:
       return state;
   }
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger),
 );
+sagaMiddleware.run(rootSaga);
+
+function* fetchPlants (){
+  try {
+    const response = yield axios.get('/api/plant');
+    const action = {
+      type: "SET_PLANTS",
+      payload: response.data
+    };
+    yield put(action);
+  }
+  catch (error) {
+    console.error("Error in GET '/api/plant'.", error);
+    alert("Error in GET. See console.");
+  }
+}
+
+function* postPlant(action){
+  try {
+    yield axios.post('/api/plant', action.payload);
+    yield put({type:"FETCH_PLANTS"});
+  }
+  catch (error) {
+    console.error("Error in POST '/api/plant'.", error);
+    alert("Error in POST. See console.");
+  }
+}
+
+function* deletePlant(action){
+  try {
+    yield axios.delete(`/api/plant/${action.payload}`);
+    yield put({type:"FETCH_PLANTS"});
+  }
+  catch (error){
+    console.error("Error in DELETE '/api/plan/:id'.", error);
+    alert("Error in DELETE. See console.");
+  }
+}
+
+function* rootSaga(){
+  yield takeEvery("FETCH_PLANTS", fetchPlants);
+  yield takeEvery("ADD_PLANT", postPlant);
+  yield takeEvery("DELETE_PLANT", deletePlant);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
